8a741225cacbb8ab754cc53930bf9e68c0c1977d
epointal <elisabeth.pointal@free.fr>
2019-01-17 16:22:37 +0100

    compress script

--- dist/manage-svg-min.js
+++ dist/manage-svg-min.js
@@ -1,1 +1,1 @@
-function formater_switch_svg(e,a){if(a)var t="fm-right",s="right";else var t="fm-left",s="left";e.parentNode.style.float=s,e.parentNode.className.indexOf(t)>=0?e.parentNode.className=e.parentNode.className.replace(t,""):e.parentNode.className=e.parentNode.className+" "+t}
+function formater_switch_svg(e,a){if(a)var t="fm-right",s="right";else t="fm-left",s="left";e.parentNode.style.float=s,e.parentNode.className.indexOf(t)>=0?e.parentNode.className=e.parentNode.className.replace(t,""):e.parentNode.className=e.parentNode.className+" "+t}
